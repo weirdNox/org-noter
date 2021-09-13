52a14db7f6b7c092a3cc358535e022134e5e7296
c1-g <char1iegordon@protonmail.com>
2021-09-13 20:03:36 +0700

    Add org-noter-nov-ov.el
    Highlight your precise notes in nov with org-noter-nov-ov.el

---
+++ other/org-noter-nov-ov.el
@@ -0,0 +1,39 @@
+;;; org-noter-nov-ov.el --- Module to highlight text in nov-mode with notes  -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2021  Charlie Gordon
+
+;; Author: Charlie Gordon <char1iegordon@protonmail.com>
+;; Keywords: multimedia
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License,
+;; any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;; 
+
+;;; Code:
+(require 'org-noter)
+(require 'nov)
+
+(defun org-noter-nov-make-ov ()
+  "TODO"
+  
+  
+  
+  )
+
+
+(provide 'org-noter-nov-ov)
+;;; org-noter-nov-ov.el ends here
+
