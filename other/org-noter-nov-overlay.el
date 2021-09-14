961fa6ae02918154d699ad24de46e4ae3aeeda8a
c1-g <char1iegordon@protonmail.com>
2021-09-14 11:28:39 +0700

    Also highlight the heading.

--- other/org-noter-nov-overlay.el
+++ other/org-noter-nov-overlay.el
@@ -57,24 +57,33 @@ Should be one of the element in `defined-colors'.")
                  (end (if (consp location-cons)
                           (cdr location-cons)
                         (1+ beg)))
-                 (ov (make-overlay beg end (org-noter--session-doc-buffer session)))
-                 (hl-color (or (org-entry-get nil org-noter-nov-overlay-color-property nil t) org-noter-nov-overlay-default-color)))
+                 (ov-pair (list (make-overlay beg end (org-noter--session-doc-buffer session))))
+                 (hl-color (or (org-entry-get nil org-noter-nov-overlay-color-property nil t)
+                               (if org-noter-insert-note-no-questions
+                                   org-noter-nov-overlay-default-color
+                                 (read-color "Highlight color: "))))
+                 (hl-color-alt (color-lighten-name hl-color 15))
+                 (action-functions (list
+                                    #'org-noter-nov-overlay-sync-current-note
+                                    #'org-noter-nov-overlay-sync-current-page-or-chapter)))
        
-       (overlay-put ov 'button ov)
-       (overlay-put ov 'category 'default-button)
-       (overlay-put ov 'face (list :background (if org-noter-insert-note-no-questions
-                                                   hl-color
-                                                 (setq hl-color (read-color "Highlight color: ")))
-                                   :foreground (readable-foreground-color hl-color)))
-       
-       (org-entry-put nil org-noter-nov-overlay-color-property hl-color)
-       
-       (overlay-put ov 'mouse-face (list :background (setq hl-color (color-lighten-name hl-color 15))
-                                         :foreground (readable-foreground-color hl-color)))
+       (save-excursion
+         (org-back-to-heading t)
+         (re-search-forward org-heading-regexp nil t)
+         (push (make-overlay (match-beginning 1) (match-end 1)) ov-pair))
 
-       (overlay-put ov 'origin (org-element-property :begin (org-element-at-point)))
+       (dolist (ov ov-pair)
+         (overlay-put ov 'button ov)
+         (overlay-put ov 'category 'default-button)
+         (overlay-put ov 'face (list :background hl-color
+                                     :foreground (readable-foreground-color hl-color)))
 
-       (overlay-put ov 'action #'org-noter-nov-overlay-sync-current-page-or-chapter)))))
+         (org-entry-put nil org-noter-nov-overlay-color-property hl-color)
+         
+         (overlay-put ov 'mouse-face (list :background hl-color-alt
+                                           :foreground (readable-foreground-color hl-color-alt)))
+
+         (overlay-put ov 'action (pop action-functions)))))))
 
 (defun org-noter-nov-make-overlay-no-question ()
   "Like `org-noter-nov-make-ov', but doesn't ask user to select the overlay color."
@@ -90,6 +99,9 @@ This wrapper ignores the first argument passed to it and just call
 `org-noter-sync-current-page-or-chapter'."
   
   (org-noter-sync-current-page-or-chapter))
+
+(defun org-noter-nov-overlay-sync-current-note (_overlay)
+  (org-noter-sync-current-note))
 
 (add-hook 'org-noter-insert-heading-hook #'org-noter-nov-make-overlay)
 
