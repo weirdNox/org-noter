aaf500bf1eb20ed5c3afa305108d6c1a770e043c
c1-g <char1iegordon@protonmail.com>
2021-12-26 10:53:04 +0700

    (feat) Add citar module.

---
+++ other/org-noter-citar.el
@@ -0,0 +1,37 @@
+;;; org-noter-citar.el --- Module for finding note files from `citar'  -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2021  c1-g
+
+;; Author: c1-g <char1iegordon@protonmail.com>
+;; Keywords: convenience
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+;;; Code:
+(require 'citar)
+
+(defun org-noter-citar-find-key-from-this-file (filename)
+  (let* ((entry-alist (mapcan (lambda (entry)
+                                (when-let ((file (citar-get-value citar-file-variable entry)))
+                                  (list (cons file (citar-get-value "=key=" entry)))))
+                              (citar--get-candidates)))
+         (key (alist-get filename entry-alist nil nil (lambda (s regexp)
+                                                        (string-match-p regexp s)))))
+    (when key
+      (concat key ".org"))))
+
+(add-to-list 'org-noter-find-additional-notes-functions #'org-noter-citar-find-key-from-this-file)
+
+(provide 'org-noter-citar)
+;;; org-noter-citar.el ends here
