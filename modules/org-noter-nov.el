4a6d1914e5c694449de9a013ed8cb106b484a044
c1-g <char1iegordon@protonmail.com>
2022-03-15 09:36:51 +0700

    (fix) document-buffer-name is undefined

--- modules/org-noter-nov.el
+++ modules/org-noter-nov.el
@@ -41,7 +41,6 @@
 
 (defun org-noter-nov-setup-handler (major-mode)
   (when (eq major-mode 'nov-mode)
-    (rename-buffer document-buffer-name)
     (advice-add 'nov-render-document :after 'org-noter--nov-scroll-handler)
     (add-hook 'window-scroll-functions 'org-noter--nov-scroll-handler nil t)
     t))
