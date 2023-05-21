e11e4ad146c11cecc90d5562fb09439b7755c387
Dmitry M <dmitrym@gmail.com>
2023-05-21 14:05:30 -0700

    Revert tests.

--- tests/org-noter-core-tests.el
+++ tests/org-noter-core-tests.el
@@ -1,4 +1,8 @@
+(add-to-list 'load-path "modules")
+(require 'org-noter)
+(require 'with-simulated-input)
 (require 'org-noter-test-utils)
+
 
 (describe "org-noter-core"
           (before-each
@@ -195,89 +199,6 @@
                                       (expect (string-match "\\:HIGHLIGHT\\:" (buffer-string))  :not :to-be nil)
                                       (expect (string-match "HARDCODED_HIGHLIGHT_LOCATION" (buffer-string))  :not :to-be nil)))))
                     )
-
-          ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-          ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-          (describe "core functionality"
-                    (before-each
-                     ;; org-noter uses file-equal-p that looks at the filesystem. We need real files to verify this functionality.
-                     (shell-command "mkdir -p /tmp/pubs/ && touch /tmp/pubs/solove-nothing-to-hide.pdf"))
-
-                    (describe "top level heading insertion"
-                              (it "can insert a top level heading at the end of the file"
-                                  (with-mock-contents
-                                   mock-contents-simple-notes-file
-                                   '(lambda ()
-                                      (org-noter--create-notes-heading "ADOCUMENT" "/tmp/file")
-                                      (expect (string-match "ADOCUMENT" (buffer-string))  :not :to-be nil)
-                                      (expect (string-match "/tmp/file" (buffer-string))  :not :to-be nil)
-                                      ;; ADOCUMENT should come after solove-nothing-to-hide
-                                      (expect (string-match "solove-nothing-to-hide" (buffer-string)) :to-be-less-than
-                                              (string-match "ADOCUMENT" (buffer-string)))
-                                      (message (buffer-string)))))
-
-                              (it "can find an existing heading without creating a new one"
-                                  (with-mock-contents
-                                   mock-contents-simple-notes-file
-                                   '(lambda ()
-                                      (let* ((found-heading-pos (org-noter--find-create-top-level-heading-for-pub "/tmp/pubs/solove-nothing-to-hide.pdf" "THIS PARAM IS UNUSED")))
-                                      (message "\n00 ----")
-                                      (message (buffer-string))
-                                      (message "\n00 ----")
-
-                                      (expect found-heading-pos :to-be 164)
-                                      (message "----")
-                                      (message (buffer-string))
-                                      (message "---- %s" (length (buffer-string)))))))
-
-
-                              (it "can create a new heading"
-                                  (with-mock-contents
-                                   mock-contents-simple-notes-file
-                                   '(lambda ()
-                                      (expect
-                                       ;; org-noter-test-file is defined in test-utils.
-                                       (org-noter--find-create-top-level-heading-for-pub "/tmp/some-other-pdf-file.pdf" "SOME HEADING")
-                                       :to-be 185)
-                                      (message "----")
-                                      (message (buffer-string))
-                                      (message "---- %s" (length (buffer-string)))
-
-                                      )))
-                              )
-
-
-                    (describe "identifying top level headlines"
-                              (before-each
-                               ;; org-noter uses file-equal-p that looks at the filesystem. We need real files to verify this functionality.
-                               (shell-command "mkdir -p /tmp/pubs/ && touch /tmp/pubs/solove-nothing-to-hide.pdf")
-                               )
-
-                              (it "can find the top level headline for a specified document and return the position"
-                                  (with-mock-contents
-                                   mock-contents-simple-notes-file
-                                   '(lambda ()
-                                      (expect
-                                       (org-noter--find-top-level-heading-for-publication-path "/tmp/pubs/solove-nothing-to-hide.pdf")
-                                       :to-be 164)
-                                      (message (buffer-string)))))
-
-
-                              (it "return nil for a non existent top level heading"
-                                  (with-mock-contents
-                                   mock-contents-simple-notes-file
-                                   '(lambda ()
-                                      (expect
-                                       (org-noter--find-top-level-heading-for-publication-path "/FAKE/PATH/DOESNT/EXIST")
-                                       :to-be nil)
-                                      (message (buffer-string)))))
-                              )
-
-
-
-
-                    )
-
 
 
 
